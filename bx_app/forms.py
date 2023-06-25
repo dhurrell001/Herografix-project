f564acfdd7b9cc025166c2c9263591faaff42599
David Hurrell <dhurrell@hotmail.co.uk>
2023-06-25 10:02:51 +0100

    Create new answer css page
    re designed question answer page. Practiced using second style shee
    t in base template.

--- bx_app/forms.py
+++ bx_app/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class AnswerForm(forms.Form):
-    answer_text = forms.CharField(widget=forms.Textarea(attrs={'rows': 8, 'cols': 75}),label='')
+    answer_text = forms.CharField(widget=forms.Textarea(attrs={'rows': 12, 'cols': 75}),label='')
