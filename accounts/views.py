6188494525d0b3b0d4168ee9b58d0e31b32ad0de
David Hurrell <dhurrell@hotmail.co.uk>
2023-06-29 20:54:41 +0100

    added signup and password reset

--- accounts/views.py
+++ accounts/views.py
@@ -9,9 +9,11 @@ from .forms import SignUpForm
 from django.urls import reverse_lazy
 from django.views import generic
 
+from django.contrib.auth.models import User
+
 
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = SignUpForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
 
