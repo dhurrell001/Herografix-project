070cb0b6dc8ed6e02dbcdeb164e065b8cb2f9c5b
David Hurrell <dhurrell@hotmail.co.uk>
2023-06-29 17:03:49 +0100

    Sign up added

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,18 @@ from django.shortcuts import render, redirect
 
 from .forms import SignUpForm
 
+
+from django.urls import reverse_lazy
+from django.views import generic
+
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
+
+
+"""
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
@@ -15,3 +27,4 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
+    """
