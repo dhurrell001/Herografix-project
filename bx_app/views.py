9cd49219c3d73105a7812c162cbf35bdc0584b63
David Hurrell <dhurrell@hotmail.co.uk>
2023-06-30 15:46:58 +0100

    add signup and reset
    Added new sign up form and logic, password reset. Added requirements.txt
    redesigned index page

--- bx_app/views.py
+++ bx_app/views.py
@@ -1,7 +1,5 @@
 
 
-
-
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .models import Question, Answer
@@ -13,50 +11,56 @@ from django.urls import reverse_lazy
 from django.http import HttpResponse
 from django.utils import timezone
 
+
 def index(request):
     user = request.user
-    context = {'user':user}
-    return render(request,'index.html',context )
-
+    context = {'user': user}
+    return render(request, 'index.html', context)
 
 
 @login_required  # Require user to be logged in
 def answer_question(request, question_id):
-    max_quetion = 3
+    max_quetion = 4
     if question_id <= max_quetion:
         question = Question.objects.get(question_identifier=question_id)
         questtext = question.question_text
-        if request.method == 'POST': # runs if form has been filled in else it will render an empty form
+        if request.method == 'POST':  # runs if form has been filled in else it will render an empty form
             form = AnswerForm(request.POST)
             if form.is_valid():
                 answer_text = form.cleaned_data['answer_text']
                 answer_date = datetime.now()
-                answer = Answer(question=question, user=request.user, answer_text=answer_text,answer_date = answer_date)  # Link answer to user
-                
+                answer = Answer(question=question, user=request.user,
+                                answer_text=answer_text, answer_date=answer_date)  # Link answer to user
+
                 answer.save()
                 return redirect('success_page')  # Redirect to a success page
         else:
             form = AnswerForm()
-            #change back to answer.html if messing around does not work!!
-        return render(request, 'quest_revamp.html', {'form': form, 'question': question,'title':questtext})
+            # change back to answer.html if messing around does not work!!
+        return render(request, 'quest_revamp.html', {'form': form, 'question': question, 'title': questtext})
     else:
         return redirect('success_page')
- 
+
 # View to show succesfful completion of answer form
+
+
 def success_page(request):
     user = request.user.username
-    context = {'user':user}
-    return render(request, 'success_page.html',context)
+    context = {'user': user}
+    return render(request, 'success_page.html', context)
 
 
-
-#@login_required # use the login_required decorator to ensure the user is authenticated
+# @login_required # use the login_required decorator to ensure the user is authenticated
 # list all answers submitted by current user
 def user_answers(request):
-    user = request.user # retrieve the currently logged-in user
-    user_answers = Answer.objects.filter(user=user) # retrieve all the answers made by the user
-    context = {'user_answers': user_answers} # create a context dictionary with the user answers
-    return render(request, 'user_answers.html', context) # render the user_answers.html template with the context dictionary
+    user = request.user  # retrieve the currently logged-in user
+    # retrieve all the answers made by the user
+    user_answers = Answer.objects.filter(user=user)
+    # create a context dictionary with the user answers
+    context = {'user_answers': user_answers}
+    # render the user_answers.html template with the context dictionary
+    return render(request, 'user_answers.html', context)
+
 
 @login_required
 def organise_answers(request):
@@ -64,40 +68,47 @@ def organise_answers(request):
     # order of how they are displayed can be rearranged in display_answers.html
     try:
         user = request.user
-        subquery1 = Answer.objects.filter(user=user, question__question_identifier=1).order_by('-answer_date')[:1]
-        subquery2 = Answer.objects.filter(user=user, question__question_identifier=2).order_by('-answer_date')[:1]
-        subquery3 = Answer.objects.filter(user=user, question__question_identifier=3).order_by('-answer_date')[:1]
-        
-        quest1= subquery1[0] if subquery1 else None
-        quest2= subquery2[0] if subquery2 else None
+        subquery1 = Answer.objects.filter(
+            user=user, question__question_identifier=1).order_by('-answer_date')[:1]
+        subquery2 = Answer.objects.filter(
+            user=user, question__question_identifier=2).order_by('-answer_date')[:1]
+        subquery3 = Answer.objects.filter(
+            user=user, question__question_identifier=3).order_by('-answer_date')[:1]
+
+        quest1 = subquery1[0] if subquery1 else None
+        quest2 = subquery2[0] if subquery2 else None
         quest3 = subquery3[0] if subquery3 else None
 
-        context = {'quest1':quest1,'quest2':quest2,'quest3':quest3}
-        #change back to 'display_answers.html' for workin function
+        context = {'quest1': quest1, 'quest2': quest2, 'quest3': quest3}
+        # change back to 'display_answers.html' for workin function
 
-        return render(request, 'display_answers.html',context)
-    
+        return render(request, 'display_answers.html', context)
+
     except Exception as e:
         # return an error message
         return HttpResponse("An error occurred: " + str(e))
-    
+
+
 def answer_letter(request):
     # get the latest answer to question. filter by id and onlt store results in 1 item list [:1]. check if this item exist else replace with None
     # order of how they are displayed can be rearranged in display_answers.html
     try:
         user = request.user
-        subquery1 = Answer.objects.filter(user=user, question__question_identifier=1).order_by('-answer_date')[:1]
-        subquery2 = Answer.objects.filter(user=user, question__question_identifier=2).order_by('-answer_date')[:1]
-        subquery3 = Answer.objects.filter(user=user, question__question_identifier=3).order_by('-answer_date')[:1]
-        
-        quest1= subquery1[0] if subquery1 else None
-        quest2= subquery2[0] if subquery2 else None
+        subquery1 = Answer.objects.filter(
+            user=user, question__question_identifier=1).order_by('-answer_date')[:1]
+        subquery2 = Answer.objects.filter(
+            user=user, question__question_identifier=2).order_by('-answer_date')[:1]
+        subquery3 = Answer.objects.filter(
+            user=user, question__question_identifier=3).order_by('-answer_date')[:1]
+
+        quest1 = subquery1[0] if subquery1 else None
+        quest2 = subquery2[0] if subquery2 else None
         quest3 = subquery3[0] if subquery3 else None
 
-        context = {'quest1':quest1,'quest2':quest2,'quest3':quest3}
-        #change back to 'display_answers.html' for workin function
+        context = {'quest1': quest1, 'quest2': quest2, 'quest3': quest3}
+        # change back to 'display_answers.html' for workin function
 
-        return render(request, 'answer_letter.html',context)
+        return render(request, 'answer_letter.html', context)
 
     except Exception as e:
         # return an error message
@@ -106,10 +117,8 @@ def answer_letter(request):
 
 def most_recent_answer(request):
     user = request.user   # gets current user data
-    most_recent_answer = Answer.objects.filter(question__question_identifier =1, user=user).latest('answer_date') # geta answer object using current useer and  custom question id number
+    most_recent_answer = Answer.objects.filter(question__question_identifier=1, user=user).latest(
+        'answer_date')  # geta answer object using current useer and  custom question id number
     context = {'most_recent': most_recent_answer}
 
-    return render(request, 'most_recent.html',context)
-
-
-
+    return render(request, 'most_recent.html', context)
